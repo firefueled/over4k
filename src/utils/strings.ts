a8e7fbaa40f6716842336e37e80948498df701ec
pablo.mint <slpkvol3@gmail.com>
2018-02-24 12:08:06 -0300

    Add strings

---
+++ src/utils/strings.ts
@@ -0,0 +1,28 @@
+const strings: object = {
+  'pt-BR': {
+    enableJSNotice: 'Precisa habilitar JavaScript para rodar a app',
+    header: 'Será que seu canal tem mais de 4 mil?',
+    inputPlaceholder: 'Nome do canal',
+    noChannelsFound: 'Não encontrei canais com este nome…',
+    appTitle: 'Mais de 4 mil!!!',
+    yearHoursOK: 'Já era pivete!',
+    yearHoursAlmost: 'Vai que vai que vai!',
+    yearHoursFail: 'Ihhh… sei não hein?',
+    yearHoursLabel: 'Horas assitidas no último ano',
+    channelLogoAlt: 'Logo do canal',
+  },
+  'en-US': {
+    enableJSNotice: 'You need to enable JavaScript to run this app',
+    header: 'Is your channel over four thousand?',
+    inputPlaceholder: 'Channel name',
+    noChannelsFound: 'Couldn\'t find channels with this name…',
+    appTitle: 'Mais de 4 mil!!!',
+    yearHoursOK: 'It\'s over four thousand!',
+    yearHoursAlmost: 'Focus you chi for a little longer',
+    yearHoursFail: 'Ihhh… sei não hein?',
+    yearHoursLabel: 'Watched hours last year',
+    channelLogoAlt: 'Channel logo',
+  }
+}
+
+export default strings
